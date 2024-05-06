dee77041d5514042ff2981dc4198d66aa6359279
botprogrammer <p03goswami@gmail.com>
2024-05-06 22:46:41 +0530

    Added data fetching with infinite scroll

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,21 +1,32 @@
+/* eslint-disable react/prop-types */
 import { Grid } from '@mui/material'
-import JobCard from './JobCard'
 
-export default function JobList() {
+import JobCard from './JobCard'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+export default function JobList({ jobData, getData }) {
   return (
-    <Grid container spacing={4} py={4}>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <JobCard />
+    <InfiniteScroll
+      dataLength={jobData.jobs.length}
+      next={getData}
+      hasMore={jobData.totalCount > jobData.jobs.length}
+      loader={<h4>Loading...</h4>}
+      scrollThreshold={1}
+      endMessage={
+        <p style={{ textAlign: 'center' }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
+      <Grid container spacing={4} py={4}>
+        {jobData.jobs.map(({ jdUid }) => {
+          return (
+            <Grid key={jdUid} item xs={12} sm={6} md={4} lg={3}>
+              <JobCard />
+            </Grid>
+          )
+        })}
       </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <JobCard />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <JobCard />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <JobCard />
-      </Grid>
-    </Grid>
+    </InfiniteScroll>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,43 @@
+import { useCallback, useEffect, useState } from 'react'
+
 import { Box } from '@mui/material'
 
 import Filters from '../components/Filters'
 import JobList from '../components/JobList'
+import axios from 'axios'
 
 export default function Home() {
+  const [jobData, setJobData] = useState({ jobs: [], totalCount: 0 })
+  const [offset, setOffset] = useState(0)
+
+  const getData = useCallback(async () => {
+    const { data } = await axios.post(
+      'https://api.weekday.technology/adhoc/getSampleJdJSON',
+      {
+        limit: 25,
+        offset: offset
+      }
+    )
+
+    if (data) {
+      const updatedData = jobData.jobs.concat(data.jdList)
+      setJobData({ jobs: updatedData, totalCount: data.totalCount })
+      setOffset(updatedData.length)
+    }
+  }, [jobData, offset])
+
+  useEffect(() => {
+    if (!jobData.jobs.length) {
+      getData()
+    }
+  }, [getData, jobData])
+
   return (
     <Box className='main' sx={{ p: 4 }}>
       <Filters />
-      <JobList />
+      {jobData.jobs.length ? (
+        <JobList jobData={jobData} getData={getData} />
+      ) : null}
     </Box>
   )
 }
